1bf17dcca05085a3c78e550759031678e9b7ec02
krockode <khperkins@gmail.com>
2012-07-14 15:57:15 +1000

    spaces

--- src/main/java/com/github/krockode/slideshow/Slideshow.java
+++ src/main/java/com/github/krockode/slideshow/Slideshow.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Slideshow extends JavaPlugin {
 
-	private static final Logger log = Logger.getLogger("Minecraft");
+    private static final Logger log = Logger.getLogger("Minecraft");
 
     public void onDisable() {
         log.info("Slideshow has been disabled");
--- src/main/java/com/github/krockode/slideshow/SlideshowCommandExecutor.java
+++ src/main/java/com/github/krockode/slideshow/SlideshowCommandExecutor.java
@@ -12,16 +12,16 @@ import org.bukkit.plugin.Plugin;
 
 public class SlideshowCommandExecutor implements CommandExecutor {
 
-	// 20 server ticks per second
-	private static final int ONE_SECOND_PERIOD = 20;
-	private static final int ONE_MINUTE_PERIOD = ONE_SECOND_PERIOD * 60;
-	private Plugin plugin;
-	private List<Location> savedLocations = new ArrayList<Location>();
-	private int taskId;
+    // 20 server ticks per second
+    private static final int ONE_SECOND_PERIOD = 20;
+    private static final int ONE_MINUTE_PERIOD = ONE_SECOND_PERIOD * 60;
+    private Plugin plugin;
+    private List<Location> savedLocations = new ArrayList<Location>();
+    private int taskId;
 
-	SlideshowCommandExecutor(Plugin plugin) {
-		this.plugin = plugin;
-	}
+    SlideshowCommandExecutor(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if ((sender instanceof Player) && (args.length > 0)) {
@@ -29,35 +29,34 @@ public class SlideshowCommandExecutor implements CommandExecutor {
             sender.sendMessage("Player location: " + player.getLocation());
 
             if ("add".equals(args[0])) {
-            	savedLocations.add(player.getLocation());
+                savedLocations.add(player.getLocation());
             } else if ("run".equals(args[0])) {
-            	SlideshowRunner task = new SlideshowRunner(player, savedLocations);
+                SlideshowRunner task = new SlideshowRunner(player, savedLocations);
                 taskId = sender.getServer().getScheduler().scheduleAsyncRepeatingTask(plugin, task, 20, ONE_MINUTE_PERIOD / 6);
             } else if ("stop".equals(args[0])) {
-            	sender.getServer().getScheduler().cancelTask(taskId);
+                sender.getServer().getScheduler().cancelTask(taskId);
             }
             return true;
         }
-		return false;
-	}
+        return false;
+    }
 
     private static class SlideshowRunner implements Runnable {
 
-    	private Player player;
-    	private List<Location> locations;
-    	private int index;
+        private Player player;
+        private List<Location> locations;
+        private int index;
 
-    	public SlideshowRunner(Player player, List<Location> locations) {
-    		this.player = player;
-    		this.locations = locations;
-		}
+        public SlideshowRunner(Player player, List<Location> locations) {
+            this.player = player;
+            this.locations = locations;
+        }
 
-		public void run() {
-			if (player.isOnline() & player.getAllowFlight()) {
-				player.setFlying(true);
-				player.teleport(locations.get(index++ % locations.size()));
-			}
-		}
-
+        public void run() {
+            if (player.isOnline() & player.getAllowFlight()) {
+                player.setFlying(true);
+                player.teleport(locations.get(index++ % locations.size()));
+            }
+        }
     }
 }
